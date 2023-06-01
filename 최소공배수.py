ba7e90155cb45be1722249e92438d211531774f9
동글이 <k9377125@naver.com>
2023-06-01 13:46:33 +0900

    Create 최소공배수.py

---
+++ 최소공배수.py
@@ -0,0 +1,16 @@
+from math import gcd
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def get_lcm(numbers):
+    lcm_value = numbers[0]
+    for number in numbers[1:]:
+        lcm_value = lcm(lcm_value, number)
+    return lcm_value
+
+numbers = []
+num = int(input('숫자를 입력'))
+numbers.append(num)
+
+print(get_lcm(numbers))
