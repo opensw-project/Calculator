73b5de9269d71179fb5ae5e2af56d07bf1dc129e
동글이 <k9377125@naver.com>
2023-06-06 14:23:37 +0900

    Create 후위 표기.py

---
+++ 새로운 폴더/후위 표기.py
@@ -0,0 +1,24 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, val):
+        self.items.append(val)
+
+    def pop(self):
+        try:
+            return self.items.pop()
+        except IndexError:
+            print("Stack is empty")
+
+    def top(self):
+        try:
+            return self.items[-1]
+        except IndexError:
+            print("Stack is empty")
+
+    def __len__(self):
+        return len(self.items)
+
+    def isEmpty(self):
+        return self.__len__() == 0
